4960da2e1a3eb2d899f4d794d676b1f4e3090158
frost56k <60257048+frost56k@users.noreply.github.com>
2025-02-15 18:29:33 +0300

    add https2

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     devServer: {
       proxy: {
-        '/api': 'http://slounik.andchar.of.by/',
+        '/api': 'https://slounik.andchar.of.by/',
       },
       hot: true,
     },
