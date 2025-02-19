dd1a49a2f50751468155a245bbb18ce21cd8abbc
frost56k <60257048+frost56k@users.noreply.github.com>
2025-02-19 16:26:51 +0300

    fix app.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,9 +1,7 @@
-const path = require('path');
 
 module.exports = {
   devServer: {
     proxy: {
-      '/api': 'http://slounik.andchar.of.by/',
       '/api': 'https://slounik.andchar.of.by/',
     },
     hot: true,
